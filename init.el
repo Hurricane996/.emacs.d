2675bfac935011e778e40a4e7be0b9b2a8287ba4
Levi Willrich <levijwillrich@gmail.com>
2018-08-16 12:03:05 -0500

    Removed autogen stuff, cleaned up, and tried to make it faster

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;; this is supposed to make emacs faster idk
-(setq gc-cons-threshold 100000000)
 (let ((file-name-handler-alist nil))
+(setq gc-cons-threshold 100000000)
 ;;; use-package
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -16,7 +16,6 @@
 (eval-when-compile
   (require 'use-package)
 )
-
 (use-package dracula-theme)
 (use-package helm)
 (use-package org-journal)
@@ -37,41 +36,4 @@
 ;;;loading other config files
 (load "~/.emacs.d/general.el")
 (load "~/.emacs.d/org-mode.el")
-
-
-
-
-
-
-
-;;;auto-generated
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(package-selected-packages (quote (helm tabbar dracula-theme use-package neotree))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 )
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(package-selected-packages
-   (quote
-    (magit helm-projectile buffer-move org-journal projectile helm tabbar dracula-theme use-package neotree))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
