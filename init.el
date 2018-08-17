d2628dcadf2212df6c3c8068290c7a4820d6e7e7
Levi Willrich <levijwillrich@gmail.com>
2018-08-17 16:42:47 -0500

    Fixed org-agenda-files

--- init.el
+++ init.el
@@ -36,3 +36,18 @@
 (load "~/.emacs.d/general.el")
 (load "~/.emacs.d/org-mode.el")
 )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("/home/lani/org/todo.org")))
+ '(package-selected-packages
+   (quote
+    (magit helm-projectile projectile helm dracula-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- org-mode.el
+++ org-mode.el
@@ -2,4 +2,5 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
-(setq org-agenda-files (directory-files-recursively "~/org/" "\.org$"))
+(setq org-agenda-files '("~/org/"))
+qw
