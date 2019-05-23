18614cdb8c823684dfb92527991f580f37c2b3fb
Hurricane996 <levijwillrich@gmail.com>
2019-05-23 16:17:25 -0500

    Edited Everything

--- init.el
+++ init.el
@@ -1,7 +1,3 @@
-;;; this is supposed to make emacs faster idk
-(let ((file-name-handler-alist nil))
-(setq gc-cons-threshold 100000000)
-;;; use-package
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -15,36 +11,21 @@
 (setq use-package-always-ensure t)
 (eval-when-compile
   (require 'use-package)
-)
-(use-package dracula-theme)
-(use-package helm)
-(use-package json)
-(use-package projectile)
-(use-package helm-projectile)
-(use-package magit)
+  )
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
-;;;helm
-(global-set-key (kbd "M-x") 'helm-M-x)
-(helm-mode 1)
-;;;projectile
-(projectile-global-mode)
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
-(define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-;;;loading other config files
-(load "~/.emacs.d/general.el")
-(load "~/.emacs.d/org-mode.el")
-)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" default)))
  '(org-agenda-files (quote ("/home/lani/org/todo.org")))
  '(package-selected-packages
    (quote
-    (magit helm-projectile projectile helm dracula-theme use-package))))
+    (alect-themes alect-dark-alt alec-dark-alt sudo-edit rainbow-delimiters switch-window rainbow-mode avy magit helm-projectile projectile helm dracula-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
