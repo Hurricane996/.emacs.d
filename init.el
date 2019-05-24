c511f35b09b82322858f3f717ad234ba295b1d5d
Hurricane996 <levijwillrich@gmail.com>
2019-05-23 22:44:27 -0500

    Automagitally commited

--- init.el
+++ init.el
@@ -21,11 +21,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" default)))
+    ("5e3fc08bcadce4c6785fc49be686a4a82a356db569f55d411258984e952f194a" "7356632cebc6a11a87bc5fcffaa49bae528026a78637acd03cae57c091afd9b9" "04dd0236a367865e591927a3810f178e8d33c372ad5bfef48b5ce90d4b476481" "54f2d1fcc9bcadedd50398697618f7c34aceb9966a6cbaa99829eb64c0c1f3ca" "ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" default)))
  '(org-agenda-files (quote ("/home/lani/org/todo.org")))
  '(package-selected-packages
    (quote
-    (exwm mark-multiple expand-region swiper spaceline popup-kill-ring yasnippet-snippets yasnippet company-irony company alect-themes alect-dark-alt alec-dark-alt sudo-edit rainbow-delimiters switch-window rainbow-mode avy magit helm-projectile projectile helm dracula-theme use-package)))
+    (zenburn-theme zenburn exwm mark-multiple expand-region swiper spaceline popup-kill-ring yasnippet-snippets yasnippet company-irony company alect-themes alect-dark-alt alec-dark-alt sudo-edit rainbow-delimiters switch-window rainbow-mode avy magit helm-projectile projectile helm dracula-theme use-package)))
  '(size-indication-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
